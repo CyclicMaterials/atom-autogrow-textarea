7d0502a9b9a5eac9e9242c34973d44386b49ea89
Frederik Krautwald <fkrautwald@gmail.com>
2015-09-23 21:38:35 +0200

    docs(API): document `atom-autogrow-textarea`
    Closes #8

--- docs/api.md
+++ docs/api.md
@@ -1,5 +1,44 @@
 
 # Cyclic Materials atom-autogrow-textarea API
 
+- [`atomAutogrowTextarea`](#atomAutogrowTextarea)
+
+### <a id="atomAutogrowTextarea"></a> `atomAutogrowTextarea({DOM, props$})`
+
+`atomAutogrowTextarea` is a component containing a TEXTAREA that grows
+in height as more lines of input are entered. Unless an explicit height
+or the `maxRows` property is set, it will never scroll.
+
+Example:
+
+    atomAutogrowTextarea({DOM, props$: Rx.Observable.just({
+      maxRows: 4
+    })});
+
+### Properties
+
+The following properties is available:
+
+Property | Description | Default
+---------------|-------------|---------
+`maxRows` | The maximum rows the TEXTAREA can occupy before scrolling | 0
+`rows` | The initial rows of the TEXTAREA | 1
+
+#### Arguments:
+
+- `{DOM, props$} :: Object` A specification of: 
+    - {Function} DOM The DOM driver function.
+    - {Observable} props$ An Observable of object of properties.
+
+#### Return:
+
+*(Object)* The atomAutogrowTextarea object. The object has the following structure:
+
+    {
+      DOM: Observable,
+      id: String,
+      state$: Observable
+    }
+
 - - -
 
--- src/atom-autogrow-textarea/index.js
+++ src/atom-autogrow-textarea/index.js
@@ -5,6 +5,40 @@ import view from './view';
 
 const DIALOGUE_NAME = `atom-AutogrowTextarea`;
 
+/**
+ * `atomAutogrowTextarea` is a component containing a TEXTAREA that grows
+ * in height as more lines of input are entered. Unless an explicit height
+ * or the `maxRows` property is set, it will never scroll.
+ *
+ * Example:
+ *
+ *     atomAutogrowTextarea({DOM, props$: Rx.Observable.just({
+ *       maxRows: 4
+ *     })});
+ *
+ * ### Properties
+ *
+ * The following properties is available:
+ *
+ * Property | Description | Default
+ * ---------------|-------------|---------
+ * `maxRows` | The maximum rows the TEXTAREA can occupy before scrolling | 0
+ * `rows` | The initial rows of the TEXTAREA | 1
+ *
+ * @param {Object} {DOM, props$} A specification of:
+ *
+ *     - {Function} DOM The DOM driver function.
+ *     - {Observable} props$ An Observable of object of properties.
+ *
+ * @returns {Object} The atomAutogrowTextarea object. The object has the
+ * following structure:
+ *
+ *     {
+ *       DOM: Observable,
+ *       id: String,
+ *       state$: Observable
+ *     }
+ */
 function atomAutogrowTextarea({DOM, props$}) {
   const id = cuid();
   const actions = intent({DOM, id, dialogueName: DIALOGUE_NAME});

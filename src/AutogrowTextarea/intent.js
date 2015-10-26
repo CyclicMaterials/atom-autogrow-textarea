24f61d3c8633aef11ae93c41bebd1bd10acac1a0
Frederik Krautwald <fkrautwald@gmail.com>
2015-10-26 23:10:12 +0100

    fix(AutogrowTextarea): change `first()` of `inputValue$`
    Change `first()` to `take(1)` of `inputValue$` in *intent.js*
    to eliminate error being thrown in the Observable.
    
    Closes #56

--- src/AutogrowTextarea/intent.js
+++ src/AutogrowTextarea/intent.js
@@ -1,15 +1,16 @@
 import Rx from 'rx';
 
 function intent({DOM, id, componentName}) {
-  const selector = `${id ? `.` + id + ` ` : ``}.${componentName}_textarea`;
+  const textareaSelector =
+    `${id ? `.` + id + ` ` : ``}.${componentName}_textarea`;
 
   return {
     inputValue$: Rx.Observable.merge(
-      DOM.select(selector).observable
+      DOM.select(textareaSelector).observable
         .filter(elements => elements.length > 0)
         .map(elements => elements[0].value)
-        .first(),
-      DOM.select(selector).events(`input`)
+        .take(1),
+      DOM.select(textareaSelector).events(`input`)
         .map(e => e.target.value)
     ).startWith(``),
   };
